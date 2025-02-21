32829f220fa58c3f4d71f2847708b09a490fa6dd
Prasoon Kumar <prasoonkumar467@gmail.com>
2025-02-21 11:52:23 +0530

    Create linearSearch.c

---
+++ 21-02-2025/linearSearch.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int linearSearch(int arr[], int n, int x) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == x) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int n, x, i;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter the elements:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element to search: ");
+    scanf("%d", &x);
+
+    i = linearSearch(arr, n, x);
+
+    if (i != -1) {
+        printf("Element %d found at index %d\n",x, i);
+    } else {
+        printf("Element not found\n");
+    }
+
+    return 0;
+}
