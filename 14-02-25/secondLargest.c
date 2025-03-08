d48409584241149457030b782ae1a794ba90e2bb
Prasoon Kumar <prasoonkumar467@gmail.com>
2025-03-08 22:33:25 +0530

    Update secondLargest.c

--- 14-02-25/secondLargest.c
+++ 14-02-25/secondLargest.c
@@ -16,7 +16,7 @@ int main() {
     
     for(int i=0; i<size-1; i++){
         for(int j=0; j<size-1-i; j++){
-            if(arr[j]>arr[j+1]){
+            if(arr[j]<arr[j+1]){
                 temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
