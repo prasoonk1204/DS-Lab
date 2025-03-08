e85143f12a30ccd251c78e276d660465640c8d08
Prasoon Kumar <prasoonkumar467@gmail.com>
2025-03-08 22:33:10 +0530

    Update matrixTranspose.c

--- 14-02-25/matrixTranspose.c
+++ 14-02-25/matrixTranspose.c
@@ -27,8 +27,8 @@ int main() {
     }
     
     printf("The tansposed matrix is: \n");
-    for(int i=0; i<row; i++){
-        for(int j=0; j<col; j++){
+    for(int i=0; i<col; i++){
+        for(int j=0; j<row; j++){
             printf("%d ", arr[j][i]);
         }
         printf("\n");
